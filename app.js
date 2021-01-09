31f98d8bbe0cbbef68d685600cecc10b978b9407
iliabes <oksana14888@gmail.com>
2021-01-09 22:07:52 +0300

    hz

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 //DB Config
 const db = require('./config/keys').mongoURI
 
-// Connect tomongo
+// Connect to mongo
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {console.log('mongoDb connected')})
     .catch(e => {console.log(e)})
@@ -19,7 +19,6 @@ app.set("view engine", "ejs");
 
 
 //bodyparser
-
 app.use(express.urlencoded({extended:false}))
 
 //Routes
--- routes/user.js
+++ routes/user.js
@@ -18,10 +18,32 @@ router.get('/register',(req,res) => {
 })
 
 //register Handle
-router.post('/register',(req,res) => {
-    const { name , emeil, password, password2} = req.body
-    console.log(name)
-})
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
 
+    if (!name || !email || !password || !password2) {
+      errors.push({ msg: 'Please enter all fields' });
+    }
+    if (password != password2) {
+      errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (password.length < 6) {
+      errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+
+    if (errors.length > 0) {
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+      });
+    } else {
+        res.send('mess')
+    }
+  });
 
 module.exports = router;
